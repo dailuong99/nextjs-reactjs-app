c07d42adc3722513ba0af2ec2472609f02c51582
Dai Luong - Software Engineer FE <dai.luong@SAI-IT.COM>
2022-10-21 17:53:46 +0700

    first commit

---
+++ helpers/db-util.js
@@ -0,0 +1,26 @@
+import { MongoClient } from "mongodb";
+
+export async function connectDatabase() {
+  const client = await MongoClient.connect(
+    'mongodb+srv://maximilian:8ZO3ycZqJ23kWBQx@cluster0.ntrwp.mongodb.net/events?retryWrites=true&w=majority'
+    // "mongodb+srv://nextjs-course:123456789Dai@cluster0.npu3i.mongodb.net/events?retryWrites=true&w=majority"
+  );
+
+  return client;
+}
+
+export async function insertDocument(client, collection, document) {
+  const db = client.db();
+
+  const result = await db.collection(collection).insertOne(document);
+
+  return result;
+}
+
+export async function getAllDocuments(client, collection, sort) {
+  const db = client.db();
+
+  const documents = await db.collection(collection).find().sort(sort).toArray();
+
+  return documents;
+}
